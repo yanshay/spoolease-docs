188ed3569d67c346bd943b496d11d8c6d009e370
yanshay <yanshay>
2025-07-11 18:07:33 +0300

    use docs.spoolease.io

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -15,10 +15,10 @@ const config: Config = {
   },
 
   // Set the production url of your site here
-  url: 'https://yanshay.github.io',
+  url: 'https://docs.spoolease.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/spoolease-docs/',
+  baseUrl: '/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
